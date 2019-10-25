6fa1422543108cbdca50090cd310c55725f45d19
Mark Vogel <markmvogel@gmail.com>
2019-10-25 10:51:09 -0400

    .

--- get_hours.py
+++ get_hours.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import httplib2
+import csv
 
 
 def hours(location):
@@ -8,11 +9,19 @@ def hours(location):
     soup = BeautifulSoup(page.content, "html.parser")
     # test = soup.contents()
     # div = soup.find(id="hours")
-    h = soup.find(id="hours")
+    h = soup.find_all(id="hours")
+    print(h)
     # print(div)
     # print(soup.get_text())
-    print(h.get_text())
+    # print(h.get_text())
 
+
+def parse_hours(location):
+    page = requests.get(location)
+    soup = BeautifulSoup(page.content, "html.parser")
+    h = soup.find(id="hours")
+    p = h.find_next_sibling('p')
+    return p.get_text()
 
 
 def locations():
@@ -29,6 +38,50 @@ def locations():
 
 if __name__ == '__main__':
     manheim_locations = locations()
+    print(manheim_locations)
     # print(manheim_locations[-1])
     # test_location = str(manheim_locations[-1])
-    hours("https://publish.manheim.com/en/locations/us-locations/manheim-milwaukee.html")
+    locations_hours = []
+
+    for i in manheim_locations:
+        try:
+            locations_hours.append([i, parse_hours(i)])
+        except AttributeError:
+            continue
+    print(locations_hours)
+    print(len(locations_hours))
+    rows = zip(locations_hours)
+    with open("manheim_hours.csv", 'w') as myfile:
+        writer = csv.writer(myfile)
+        for row in rows:
+            writer.writerow(row)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
